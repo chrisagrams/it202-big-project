dd49862d4529cbce8ae74c16ae256457999daf8d
Chris Grams <cgrams2@uic.edu>
2020-05-03 18:27:17 +0000

    fixed offline caching

--- sw.js
+++ sw.js
@@ -17,6 +17,17 @@ self.addEventListener("install", (e) => {
 });
 
 
-self.addEventListener("fetch", (e) => {
-  console.log("fetch", e);
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open("gasPrice_cache").then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
